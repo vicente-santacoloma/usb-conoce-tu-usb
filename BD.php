6fc56de5c1c9e213a45d1902b29270dea0bf7dcf
Andreth Salazar <andreth@slash-pc.(none)>
2013-03-07 15:02:48 -0430

    Sintaxis de BD

--- BD.php
+++ BD.php
@@ -16,7 +16,7 @@ $pg_close($dbconexion);
 
 public function agregarElem($elem){
 $conexion = conectarse();
-$query = "INSERT INTO ".$schema.".". $elem->getTable() ." VALUES (".$elem->columnsDB().") = (". $elem->valuesDB() .")";
+$query = "INSERT INTO "."\"".$schema."\".". $elem->getTable() ." VALUES (".$elem->columnsDB().") = (". $elem->valuesDB() .")";
 $result = pg_query($conexion, $query);
 if (!$result)
 {
@@ -27,7 +27,7 @@ desconectarse($conexion);
 
 public  function eliminarElem($elem){
 $conexion = conectarse();
-$query = " DELETE FROM ".$schema.".".$elem->getTable()." WHERE id = ".$elem->getId();
+$query = " DELETE FROM "."\"".$schema."\".". $elem->getTable() ." WHERE id = ".$elem->getId();
 $result = pg_query($conexion, $query);
 if (!$result)
 {
@@ -38,7 +38,7 @@ desconectarse($conexion);
 
 public function consultarElem($elem){
 $conexion = conectarse();
-$query = " SELECT * FROM ".$schema.".".$elem->getTable()." WHERE id = ".$elem->getId();
+$query = " SELECT * FROM "."\"".$schema."\".".$elem->getTable()." WHERE id = ".$elem->getId();
 $result = pg_query($conexion, $query);
 desconectarse($conexion);
 return pg_fetch_object($result);
@@ -46,7 +46,7 @@ return pg_fetch_object($result);
 
 public function modificarElem($elem, $value){
 $conexion = conectarse();
-$query = "UPDATE ".$schema.".".$elem->getTable()." SET ".$elem->getColumn()." = ".$value." WHERE id  = ".$elem->getId();
+$query = "UPDATE "."\"".$schema."\".".$elem->getTable()." SET ".$elem->getColumn()." = ".$value." WHERE id  = ".$elem->getId();
 $result = pg_query($conexion, $query);
     if (!$result)
     {
